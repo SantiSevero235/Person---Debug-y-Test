a94902dd40f915b64b4396896bbc1b2dc593920a
Santiago Severo <santiagosevero28@gmail.com>
2022-04-24 20:41:27 -0300

    Person Debug & Test

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,47 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-            // Insertá tu código de inicialización aquí
-        }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestCorrectName() 
         {
-            // Insertá tu código  de pruebaaquí
+            Person santiago = new Person("Santiago Severo", "5.219.678-2");
+            string expected = "Soy Santiago Severo y mi cédula es 5.219.678-2";
+            string actual = $"Soy {santiago.Name} y mi cédula es {santiago.ID}";
+
+            Assert.AreEqual(expected,actual);
+        }
+        [Test]
+        public void TestCedula1()
+        {
+            string cedula = "52196782";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected,actual);    
+        }
+        [Test]
+        public void TestCedula2()
+        {
+            string cedula = "5.219.678-2";//Con puntos y guión
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected,actual);    
+        }
+        [Test]
+        public void TestCedula3()
+        {
+            string cedula = "521967823";//Mas de 8 dígitos
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected,actual);    
+        }
+        [Test]
+        public void TestCedula4()
+        {
+            string cedula = "";// Cédula vacía
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected,actual);    
         }
     }
 }
